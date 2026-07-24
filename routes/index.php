55ffb7f1a689f8f90ef25861d001772adf70446a
tung <ngtung266@gmail.com>
2026-07-24 23:05:22 +0700

    feat rooms

--- routes/index.php
+++ routes/index.php
@@ -3,7 +3,29 @@
 $action = $_GET['action'] ?? '/';
 
 match ($action) {
-    '/',
-    'movies'
-        => (new MovieController)->index(),
+    '/'                   => (new MovieController)->index(),
+
+    // Rooms
+    'rooms'               => (new RoomController)->list(),
+    'roomAdd'             => (new RoomController)->add(),
+    'roomAddProcess'      => (new RoomController)->addProcess(),
+    'roomEdit'            => (new RoomController)->edit(),
+    'roomEditProcess'     => (new RoomController)->editProcess(),
+    'roomDelete'          => (new RoomController)->delete(),
+
+    // Room Types
+    'room-types'          => (new RoomTypeController)->list(),
+    'roomTypeAdd'         => (new RoomTypeController)->add(),
+    'roomTypeAddProcess'  => (new RoomTypeController)->addProcess(),
+    'roomTypeEdit'        => (new RoomTypeController)->edit(),
+    'roomTypeEditProcess' => (new RoomTypeController)->editProcess(),
+    'roomTypeDelete'      => (new RoomTypeController)->delete(),
+
+    // Seat Types
+    'seat-types'          => (new SeatTypeController)->list(),
+    'seatTypeAdd'         => (new SeatTypeController)->add(),
+    'seatTypeAddProcess'  => (new SeatTypeController)->addProcess(),
+    'seatTypeEdit'        => (new SeatTypeController)->edit(),
+    'seatTypeEditProcess' => (new SeatTypeController)->editProcess(),
+    'seatTypeDelete'      => (new SeatTypeController)->delete(),
 };
